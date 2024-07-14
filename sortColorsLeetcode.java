20b06397c207fd6e3a48585cef55c25e9565b2d3
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-07-14 15:39:23 +0530

    Create sortColorsLeetcode.java

---
+++ sortColorsLeetcode.java
@@ -0,0 +1,13 @@
+/**
+ * sortColorsLeetcode
+ */
+import java.util.Arrays;
+public class sortColorsLeetcode {
+    public static void sortColor(int nums[]){
+        Arrays.sort(nums);
+    }
+    public static void Main(String[] args) {
+        int nums[] ={2,0,2,1,1,0};
+        sortColor(nums);
+}
+}
