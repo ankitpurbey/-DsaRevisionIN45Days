28da69e2f3412be94f2ed9dd6be765a226d61ca6
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-07-15 21:24:04 +0530

    Create invertTree.java

---
+++ BinaryTree/invertTree.java
@@ -0,0 +1,13 @@
+package BinaryTree;
+
+public class invertTree {
+    public class Node{
+        int data;
+        Node left,right;
+        public Node(int data){
+            this.data =data;
+            this.left =null;
+            this.right==null
+        }
+    }
+}
