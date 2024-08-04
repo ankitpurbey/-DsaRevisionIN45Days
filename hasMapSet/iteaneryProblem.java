9b14f41fa6534fb8fddaf875c190971fcdcd1440
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-05 01:15:32 +0530

    Create iteaneryProblem.java

---
+++ hasMapSet/iteaneryProblem.java
@@ -0,0 +1,17 @@
+package hasMapSet;
+
+import java.util.HashMap;
+
+public class iteaneryProblem {
+
+    public static String getStart(HashMap<String,String> tick){
+        
+    }
+    public static void main(String[] args) {
+        HashMap<String,String> hm = new HashMap<>();
+        hm.put("chennai", "bengalur");
+        hm.put("Mumbai", "Delhi");
+        hm.put("Goa", "chennai");
+        hm.put("delhi", "mumbai");
+    }
+}
