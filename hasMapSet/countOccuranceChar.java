64f4aba17ea0e861fd5f62333e24330dba37f2c4
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-07 09:53:05 +0530

    Update countOccuranceChar.java

--- hasMapSet/countOccuranceChar.java
+++ hasMapSet/countOccuranceChar.java
@@ -7,15 +7,14 @@ public class countOccuranceChar {
         countChar("Test Automation java");
     }
     public static void countChar(String s){
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String c : s.split(" ")){
+        HashMap<String,Integer> map = new HashMap<>();
+        for( String c : s.split(" ")){
             if(map.containsKey(c)){
-                map.put(c, map.get(c)+1);
+                map.put(c, map.get(c) +1);
             }
             else{
                 map.put(c, 1);
             }
-
         }
         System.out.println(map);
     }
