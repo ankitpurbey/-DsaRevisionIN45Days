6d16d0753fdac3936465ae3d51c7da007d559cf6
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-10 01:01:54 +0530

    Update replaceChar.java

--- String/replaceChar.java
+++ String/replaceChar.java
@@ -2,25 +2,35 @@ package String;
 import java.util.Scanner;
 public class replaceChar {
 
-    public static void replaceChar(String str, char ch1, char ch2){
-        int n = str.length();
-        for(int i=0; i<n; i++  ){
-            Character ch = str.charAt(i);
-            if(str.charAt(i) == ch1){
-                ch = ch2;
-            }
-            else if( str.charAt(i) == ch2){
-                ch= ch1;
+    
+
+
+    public static void replaceCharacter(char[] str, int n, char ch1, char ch2) {
+        for (int i = 0; i < n; i++) {
+            if (str[i] == ch1) {
+                str[i] = ch2;
+            } else if (str[i] == ch2) {
+                str[i] = ch1;
             }
         }
         System.out.println(str);
     }
+
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
-        String str = sc.next();
-        char ch1 = 'a';
-        char ch2 ='p';
-        replaceChar(str, ch1, ch2);
-        
+        Scanner scanner = new Scanner(System.in);
+        char[] a = new char[100];
+        char b, c;
+        int len;
+
+        String input = scanner.nextLine();
+        a = input.toCharArray();
+        b = scanner.next().charAt(0);
+        c = scanner.next().charAt(0);
+
+        len = a.length;
+        replaceCharacter(a, len, b, c);
+
+        scanner.close();
     }
 }
+
