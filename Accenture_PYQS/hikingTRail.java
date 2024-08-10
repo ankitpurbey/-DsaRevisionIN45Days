5e4d8475fa426b5f143644073f38256fe4638ec7
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-11 01:02:57 +0530

    Update hikingTRail.java

--- Accenture_PYQS/hikingTRail.java
+++ Accenture_PYQS/hikingTRail.java
@@ -6,22 +6,22 @@ public class hikingTRail {
         int peekPoint =0;
         
         for(int i=0;i<n-1; i++){
-
             if(n<=2){
             return Math.max(arr[i], arr[i+1]);
             }
             if(arr[i] > arr[i+1]){
                 peekPoint =arr[i];
+                break;
             }
+            
             
         }
         return peekPoint;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-       
-        int arr[] = {1,2,3,4,3,2,1};
-        int n = arr.length;
+       int n = sc.nextInt();
+        int arr[] = new int[n];
         System.out.println(hikinPoint(n, arr));
 
     }
