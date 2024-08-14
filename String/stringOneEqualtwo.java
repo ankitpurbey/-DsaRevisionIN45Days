f5b00386c7b55f3c1ce8e938d2493713ff54089b
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-14 16:00:44 +0530

    Create stringOneEqualtwo.java

---
+++ String/stringOneEqualtwo.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.HashSet;
+
+public class stringOneEqualtwo {
+    public static int countChangedStr(String str, String str2){
+        HashSet<Character> hs = new HashSet<>();
+        int count =0;
+        for(int i=0; i<str2.length(); i++){
+            hs.add(str2.charAt(i));
+        }
+        for(int i=0; i<str.length(); i++){
+            if(hs.contains(str.charAt(i))){
+               count =count+count;
+            }
+            
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        String str ="ABD";
+        String str2 ="AABCCAD";
+        System.out.println(countChangedStr(str, str2));
+    }
+}
