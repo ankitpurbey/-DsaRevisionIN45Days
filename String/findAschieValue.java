3ab483872acf09f4bb330300f502bf0a6329cda3
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-17 00:58:30 +0530

    Create findAschieValue.java

---
+++ String/findAschieValue.java
@@ -0,0 +1,9 @@
+package String;
+
+public class findAschieValue {
+    public static void main(String[] args) {
+        char c ='A';
+        int aschie = c;
+        System.out.println("ASCII Value will be :" + aschie);
+    }
+}
