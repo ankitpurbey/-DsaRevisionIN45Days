8f9d307c7137f6e3a6eb70722af64bd52dc76c79
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-17 01:13:08 +0530

    Update sumOfDigitInString.java

--- String/sumOfDigitInString.java
+++ String/sumOfDigitInString.java
@@ -2,13 +2,18 @@ package String;
 
 public class sumOfDigitInString {
     public static int countDigit(String str){
+        String tempSum ="0";
         int count =1;
         for(int i=0; i<str.length(); i++){
             if(Character.isDigit(str.charAt(i))){
                 count = count + str.charAt(i);
             }
+            else{
+                count += Integer.parseInt(tempSum);
+                tempSum ="0";
+            }
         }
-        return count;
+        return count +Integer.parseInt(tempSum);
     }
     public static void main(String[] args) {
         String str ="Ankit321";
