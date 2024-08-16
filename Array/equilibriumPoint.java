ddc2ad89b812ac3387b4c184dd8b12d13d2234d8
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-16 20:14:15 +0530

    Update equilibriumPoint.java

--- Array/equilibriumPoint.java
+++ Array/equilibriumPoint.java
@@ -1,15 +1,27 @@
 public class equilibriumPoint {
     public static int equilibriumIndex(int arr[]){
-        if(arr.length%2 !=0){
-            return -1;
+        int n = arr.length;
+        int suffix[] =new int[n];
+        int prefix[] = new int[n];
+        prefix[0] = suffix[0];
+        prefix[n-1] = suffix[n-1];
+
+
+        for(int i=1; i<arr.length; i++){
+            prefix[i] = arr[i] + prefix[i-1];
+            suffix[n-i-1] = arr[n-i-1] + suffix[n-i];
+            
         }
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length; j++){
-                
+        for(int i=0; i<n; i++){
+            if(suffix[i] ==prefix[i]){
+                return i;
             }
         }
+        return -1;
+        
     }
     public static void main(String[] args) {
-        int arr[] ={1,2,3,3};
+        int arr[] ={-7,1,5,2,-4,3,0};
+        System.out.println(equilibriumIndex(arr));
     }
 }
