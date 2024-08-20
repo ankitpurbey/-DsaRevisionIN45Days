6299556ac8a6f54dc3805897920d111ec7132b6e
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-20 20:40:38 +0530

    Create alphabetIncoding.java

---
+++ String/alphabetIncoding.java
@@ -0,0 +1,16 @@
+package String;
+
+public class alphabetIncoding {
+    public static String convertToAlphabet(String str){
+        int count =0;
+        String ans ="";
+        for(int i=0; i<str.length(); i++){
+            
+        }
+
+    }
+    public static void main(String[] args) {
+        String str ="11101101111";
+        
+    }
+}
