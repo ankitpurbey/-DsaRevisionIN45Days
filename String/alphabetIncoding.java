a438e948331350fdcccbd5c3d0ec60087fc815a6
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-20 20:43:07 +0530

    Update alphabetIncoding.java

--- String/alphabetIncoding.java
+++ String/alphabetIncoding.java
@@ -5,7 +5,14 @@ public class alphabetIncoding {
         int count =0;
         String ans ="";
         for(int i=0; i<str.length(); i++){
-            
+            if(str.charAt(i) =='1'){
+                count++;
+            }
+            else{
+                if (count>0) {
+                    ans += (char)(64+count);
+                }
+            }
         }
 
     }
