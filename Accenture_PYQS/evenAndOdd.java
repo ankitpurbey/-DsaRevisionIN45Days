be73fa55b657a9f519d2c426b7374f1befed850e
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-09-01 15:41:19 +0530

    .

---
+++ Accenture_PYQS/evenAndOdd.java
@@ -0,0 +1,9 @@
+package Accenture_PYQS;
+
+public class evenAndOdd {
+    public stat
+    public static void main(String[] args) {
+        int n =11;
+        
+    }
+}
---
+++ Accenture_PYQS/pythaorean.java
@@ -0,0 +1,11 @@
+public class pythaorean {
+    public static int findPythagoran(int a , int b){
+        
+        return Math.sqrt(a*a +b*b);
+    }
+    public static void main(String[] args) {
+        int a =2;
+        int b =3;
+        System.out.println(findPythagoran(a, b));
+    }
+}
