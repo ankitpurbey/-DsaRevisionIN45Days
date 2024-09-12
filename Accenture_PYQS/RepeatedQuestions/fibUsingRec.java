46fca633d83031e6e79f9b6148e2dacfa7eb6ce4
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-09-13 00:39:41 +0530

    Update fibUsingRec.java

--- Accenture_PYQS/RepeatedQuestions/fibUsingRec.java
+++ Accenture_PYQS/RepeatedQuestions/fibUsingRec.java
@@ -1,7 +1,17 @@
 package RepeatedQuestions;
-
+import java.util.Scanner;
 public class fibUsingRec {
+    public int fib(int n){
+        if(n<=1){
+            return n;
+        }
+       return fib(n-1) + fib(n-2);
+    }
+
     public static void main(String[] args) {
-        Scanner sc 
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+
     }
 }
