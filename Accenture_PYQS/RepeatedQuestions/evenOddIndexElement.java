0c570c751f40fa85b2afed4cdb39ddd1a13ef921
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-09-13 01:06:24 +0530

    Update evenOddIndexElement.java

--- Accenture_PYQS/RepeatedQuestions/evenOddIndexElement.java
+++ Accenture_PYQS/RepeatedQuestions/evenOddIndexElement.java
@@ -1,11 +1,16 @@
 package RepeatedQuestions;
-
+import java.util.Scanner;
 public class evenOddIndexElement {
     public static void main(String[] args) {
-        int arr[] ={1,2,3,4,5,6};
+        Scanner sc = new Scanner (System.in);
+        int n  = sc.nextInt();
+        int []arr = new int[n];
+
+
         int oddIndex =0;
         int evenIndex =0;
         for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
             if(i%2==0){
                 evenIndex += arr[i];
                 
